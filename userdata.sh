e7700f79ed2833ce711101cf3b69254b38d3ab8d
Jonathan Galdino <johnzballad@gmail.com>
2023-04-04 01:04:15 -0300

    this shit will do the job

--- userdata.sh
+++ userdata.sh
@@ -3,14 +3,23 @@
 # Update the package manager
 sudo yum update -y
 
+# Install Git
+sudo yum install -y nodejs
+
 # Install Node.js LTS
 curl -sL https://rpm.nodesource.com/setup_lts.x | sudo bash -
 sudo yum install -y nodejs
 
+# Install PM2
+npm i -g pm2
+
 # Clone the Git repository
 git clone https://github.com/coderonfleek/simple-node-api.git /home/ec2-user/app
 
-# Install dependencies and run the app
+# Install dependencies
 cd /home/ec2-user/app
 npm install
-npm start
+
+# Run the app
+pm2 start server.js
+
